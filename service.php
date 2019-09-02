64ee03295fe281c577e5a32df2508b3d07347a19
kumahacker <kumahavana@gmail.com>
2019-09-01 20:50:11 -0400

    fix

--- service.php
+++ service.php
@@ -14,6 +14,7 @@ class SudokuService extends ApretasteService
      */
     public function _main()
     {
+        $this->response->setLayout('sudoku.ejs');
         $sudoku = [];
         for ($i = 0; $i < 81; $i++) {
             $sudoku[] = 0;
